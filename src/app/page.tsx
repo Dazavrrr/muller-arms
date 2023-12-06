ae6bd9084b1025dcb60463782e238453b0dd8efa
Dazavrrr <damelasha@gmail.com>
2023-12-06 17:04:57 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+ 
+import Header from 'src/components/Header'
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+    <Header />
+    </main>
   )
 }
