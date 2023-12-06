38e23e512a0d8003bc549e27faf29af3bbbc77ce
Dazavrrr <damelasha@gmail.com>
2023-12-06 17:04:52 +0200

    Create index.tsx

---
+++ src/app/pages/Page/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from 'src/components/Header';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hello Next.js</h1>
+    </div>
+  );
+};
+
+export default HomePage;
