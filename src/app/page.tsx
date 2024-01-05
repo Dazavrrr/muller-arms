3faf99b9eed6c6901bee905ef6b33620d6393995
Dazavrrr <damelasha@gmail.com>
2024-01-05 16:16:59 +0200

    Created Instructors and Advantages sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import { MontserratFont, DuskDemon } from 'src/common/fonts'
 import Hero from "@/components/Hero"
 import '@/styles/globals.scss'
 import Services from '@/components/Services'
+import Instructors from '@/components/Instructors'
+import Advantages from '@/components/Advantages'
 
 export default function Home() {
   return <div>
     <Hero />
     <Services />
+    <Instructors />
+    <Advantages />
   </div>
 }
