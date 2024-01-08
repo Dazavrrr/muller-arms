a5409b7f23a82147310ac0f660569fdade057b65
Yuriy Soproniuk <sopronukCC@gmail.com>
2024-01-08 21:13:19 +0200

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { MontserratFont, DuskDemon } from 'src/common/fonts'
 import Hero from "@/components/Hero"
 import '@/styles/globals.scss'
 import Services from '@/components/Services'
--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,106 +1,110 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Image from 'next/image'
-import ServicesPhoto1 from 'public/images/servicesPhoto1.png'
-import ServicesPhoto2 from 'public/images/servicesPhoto2.png'
-import ServicesPhoto3 from 'public/images/servicesPhoto3.png'
-import ServicesPhoto4 from 'public/images/servicesPhoto4.png'
-
 
 const Services = () => {
-  return <div className={styles.services}>
-    <h1 className={styles.services_title}>Наші послуги</h1>
-    <p className={styles.services_text}>Навчайтесь та здобувайте безцінний досвід зі стрільби</p>
+  return (
+    <div className={styles.services}>
+      <h1 className={styles.services_title}>Наші послуги</h1>
+      <p className={styles.services_text}>
+        Навчайтесь та здобувайте безцінний досвід зі стрільби
+      </p>
 
-    <div className={styles.services_cards}>
+      <div className={styles.services_cards}>
+        <div className={styles.services_card}>
+          <div className={styles.services_photo}>
+            <Image
+              className={styles.services_image}
+              src="/images/servicesPhoto1.png"
+              alt="Базове тренування з автоматом"
+              width={267}
+              height={200}
+            />
+          </div>
+          <div className={styles.services_info}>
+            <h1 className={styles.services_infoTitle}>
+              Базове тренування з автоматом
+            </h1>
+            <div className={styles.services_infoPrice}>
+              <p>ВАРТІСТЬ:</p>
+              <p className={styles.services_infoPriceOrange}>1 400 ГРН</p>
+            </div>
+            <div className={styles.services_infoBtn}>
+              <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+            </div>
+          </div>
+        </div>
 
-      <div className={styles.services_card}>
-        <div className={styles.services_photo}>
-          <Image
-            className={styles.services_image}
-          src={ServicesPhoto1}
-          alt='Базове тренування з автоматом'
-          width={267}
-          height={200}
-        />
-        </div>
-        <div className={styles.services_info}>
-          <h1 className={styles.services_infoTitle}>Базове тренування з автоматом</h1>
-          <div className={styles.services_infoPrice}>
-            <p>ВАРТІСТЬ:</p>
-            <p className={styles.services_infoPriceOrange}>1 400 ГРН</p>
+        <div className={styles.services_card}>
+          <div className={styles.services_photo}>
+            <Image
+              className={styles.services_image}
+              src="/images/servicesPhoto2.png"
+              alt="Групові тренування"
+              width={267}
+              height={200}
+            />
           </div>
-          <div className={styles.services_infoBtn}>
-            <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.services_card}>
-        <div className={styles.services_photo}>
-          <Image
-            className={styles.services_image}
-          src={ServicesPhoto2}
-          alt='Групові тренування'
-          width={267}
-          height={200}
-        />
-        </div>
-        <div className={styles.services_info}>
-          <h1 className={styles.services_infoTitle}>Групові тренування</h1>
-          <div className={styles.services_infoPrice}>
-            <p>ВАРТІСТЬ:</p>
-            <p className={styles.services_infoPriceOrange}>1 500 ГРН</p>
-          </div>
-          <div className={styles.services_infoBtn}>
-            <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+          <div className={styles.services_info}>
+            <h1 className={styles.services_infoTitle}>Групові тренування</h1>
+            <div className={styles.services_infoPrice}>
+              <p>ВАРТІСТЬ:</p>
+              <p className={styles.services_infoPriceOrange}>1 500 ГРН</p>
+            </div>
+            <div className={styles.services_infoBtn}>
+              <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.services_card}>
-        <div className={styles.services_photo}>
-          <Image
-            className={styles.services_image}
-          src={ServicesPhoto3}
-          alt='Індивідуальні тренування'
-          width={267}
-          height={200}
-        />
-        </div>
-        <div className={styles.services_info}>
-          <h1 className={styles.services_infoTitle}>Індивідуальні тренування</h1>
-          <div className={styles.services_infoPrice}>
-            <p>ВАРТІСТЬ:</p>
-            <p className={styles.services_infoPriceOrange}>1 500 ГРН</p>
+        <div className={styles.services_card}>
+          <div className={styles.services_photo}>
+            <Image
+              className={styles.services_image}
+              src="/images/servicesPhoto3.png"
+              alt="Індивідуальні тренування"
+              width={267}
+              height={200}
+            />
           </div>
-          <div className={styles.services_infoBtn}>
-            <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+          <div className={styles.services_info}>
+            <h1 className={styles.services_infoTitle}>
+              Індивідуальні тренування
+            </h1>
+            <div className={styles.services_infoPrice}>
+              <p>ВАРТІСТЬ:</p>
+              <p className={styles.services_infoPriceOrange}>1 500 ГРН</p>
+            </div>
+            <div className={styles.services_infoBtn}>
+              <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.services_card}>
-        <div className={styles.services_photo}>
-          <Image
-            className={styles.services_image}
-          src={ServicesPhoto4}
-          alt='КУРC МАЙБУТНЬОГО БІЙЦЯ'
-          width={267}
-          height={200}
-        />
-        </div>
-        <div className={styles.services_info}>
-          <h1 className={styles.services_infoTitle}>КУРC МАЙБУТНЬОГО БІЙЦЯ</h1>
-          <div className={styles.services_infoPrice}>
-            <p>ВАРТІСТЬ:</p>
-            <p className={styles.services_infoPriceOrange}>9 900 ГРН</p>
+        <div className={styles.services_card}>
+          <div className={styles.services_photo}>
+            <Image
+              className={styles.services_image}
+              src="/images/servicesPhoto4.png"
+              alt="КУРC МАЙБУТНЬОГО БІЙЦЯ"
+              width={267}
+              height={200}
+            />
           </div>
-          <div className={styles.services_infoBtn}>
-            <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+          <div className={styles.services_info}>
+            <h1 className={styles.services_infoTitle}>
+              КУРC МАЙБУТНЬОГО БІЙЦЯ
+            </h1>
+            <div className={styles.services_infoPrice}>
+              <p>ВАРТІСТЬ:</p>
+              <p className={styles.services_infoPriceOrange}>9 900 ГРН</p>
+            </div>
+            <div className={styles.services_infoBtn}>
+              <a href="">ДІЗНАТИСЬ БІЛЬШЕ</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  )
 }
 
 export default Services
