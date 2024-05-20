d1f7b5a8b339a7b7f482ff6926f114dcf25c6fe2
Dazavrrr <damelasha@gmail.com>
2024-05-20 15:44:34 +0300

    updated main page adaptive

--- src/components/Instagram/index.tsx
+++ src/components/Instagram/index.tsx
@@ -1,6 +1,6 @@
 //libs
-import React from "react";
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 //styles
 import styles from './styles.module.scss'
 //images
@@ -9,50 +9,56 @@ import photo2 from '../../../public/images/instagram/instagram-photo2.webp'
 import photo3 from '../../../public/images/instagram/instagram-photo3.webp'
 import photo4 from '../../../public/images/instagram/instagram-photo4.webp'
 
-
 const Instagram = () => {
-  return <section className={styles.instagram}>
-    <div className={styles.instagram_wrapper}>
-      <h2 className={styles.instagram_title}>НАШ INSTAGRAM</h2>
-    
-    <div className={styles.instagram_photos}>
-      <Image
-        className={styles.instagram_photo}
-        src={photo1}
-        width={267}
-        height={268}
-        placeholder={'blur'}
-        alt="Muller Arms Instagram Photo"
-      />
-      <Image
-        className={styles.instagram_photo}
-        src={photo2}
-        width={279}
-        height={270}
-        placeholder={'blur'}
-        alt="Muller Arms Instagram Photo"
-      />
-      <Image
-        className={styles.instagram_photo}
-        src={photo3}
-        width={279}
-        height={270}
-        placeholder={'blur'}
-        alt="Muller Arms Instagram Photo"
-      />
-      <Image
-        className={styles.instagram_photo}
-        src={photo4}
-        width={279}
-        height={270}
-        placeholder={'blur'}
-        alt="Muller Arms Instagram Photo"
-      />
-    </div>
+  return (
+    <section className={styles.instagram}>
+      <div className={styles.instagram_wrapper}>
+        <h2 className={styles.instagram_title}>НАШ INSTAGRAM</h2>
 
-    <a className={styles.instagram_btn} href="https://www.instagram.com/mullerarms/">ПЕРЕЙТИ В INSTAGRAM</a>
-    </div>
-  </section>
+        <div className={styles.instagram_photos}>
+          <Image
+            className={styles.instagram_photo}
+            src={photo1}
+            width={267}
+            height={268}
+            placeholder={'blur'}
+            alt="Muller Arms Instagram Photo"
+          />
+          <Image
+            className={styles.instagram_photo}
+            src={photo2}
+            width={279}
+            height={270}
+            placeholder={'blur'}
+            alt="Muller Arms Instagram Photo"
+          />
+          <Image
+            className={styles.instagram_photo}
+            src={photo3}
+            width={279}
+            height={270}
+            placeholder={'blur'}
+            alt="Muller Arms Instagram Photo"
+          />
+          <Image
+            className={styles.instagram_photo}
+            src={photo4}
+            width={279}
+            height={270}
+            placeholder={'blur'}
+            alt="Muller Arms Instagram Photo"
+          />
+        </div>
+
+        <a
+          className={styles.instagram_btn}
+          href="https://www.instagram.com/mullerarms/"
+        >
+          ПЕРЕЙТИ В INSTAGRAM
+        </a>
+      </div>
+    </section>
+  )
 }
 
 export default Instagram
