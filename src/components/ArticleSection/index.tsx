fca00eb6f6a33715f93847baf005859e3376fc66
Dazavrrr <damelasha@gmail.com>
2024-05-20 18:52:28 +0300

    n

--- src/components/ArticleSection/index.tsx
+++ src/components/ArticleSection/index.tsx
@@ -1,7 +1,10 @@
 //libs
 import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
 //styles
 import styles from './styles.module.scss'
+import '@/styles/swiper.scss'
+//types
 import { SectionResponse } from '@/common/types'
 
 const ArticleSection = ({ section }: { section: SectionResponse }) => {
@@ -9,33 +12,51 @@ const ArticleSection = ({ section }: { section: SectionResponse }) => {
   return (
     <section className={styles.section}>
       <div className={styles.wrapper}>
-        <div className={styles.images_wrapper_mob}>
-          {images.map((image, key) => (
-            <div className={styles.image_wrapper} key={key}>
-              <Image
-                className={styles.image}
-                src={image}
-                alt="MullerArms"
-                width={280}
-                height={311}
-              />
-            </div>
-          ))}
+        <div className={styles.text_wrapper}>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.text}>{text}</p>
         </div>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.text}>{text}</p>
+
         <div className={styles.images_wrapper}>
-          {images.map((image, key) => (
-            <div className={styles.image_wrapper} key={key}>
-              <Image
-                className={styles.image}
-                src={image}
-                alt="MullerArms"
-                width={280}
-                height={311}
-              />
-            </div>
-          ))}
+          {images.length > 1 ? (
+            <Swiper
+              loop
+              spaceBetween={20}
+              className="mySwiper"
+              slidesPerView={'auto'}
+            >
+              {images.map((image, key) => (
+                <SwiperSlide key={key}>
+                  <div className={styles.image_wrapper}>
+                    <Image
+                      className={styles.image}
+                      src={image}
+                      alt="MullerArms"
+                      width={280}
+                      height={311}
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <>
+              {images.map((image, key) => (
+                <div
+                  className={`${styles.image_wrapper} ${styles.single_img_wrapper}`}
+                  key={key}
+                >
+                  <Image
+                    className={styles.image}
+                    src={image}
+                    alt="MullerArms"
+                    width={280}
+                    height={311}
+                  />
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>
--- src/components/Instagram/index.tsx
+++ src/components/Instagram/index.tsx
@@ -1,8 +1,11 @@
+'use client'
 //libs
 import React from 'react'
 import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
 //styles
 import styles from './styles.module.scss'
+import '@/styles/swiper.scss'
 //images
 import photo1 from '../../../public/images/instagram/instagram-photo1.webp'
 import photo2 from '../../../public/images/instagram/instagram-photo2.webp'
@@ -15,49 +18,61 @@ const Instagram = () => {
       <div className={styles.instagram_wrapper}>
         <h2 className={styles.instagram_title}>НАШ INSTAGRAM</h2>
 
-        <div className={styles.instagram_photos}>
-          <div className={styles.img_wrapper}>
-            <Image
-              className={styles.instagram_photo}
-              src={photo1}
-              width={267}
-              height={268}
-              placeholder={'blur'}
-              alt="Muller Arms Instagram Photo"
-            />
-
-          </div>
-          <div className={styles.img_wrapper}>
-            <Image
-              className={styles.instagram_photo}
-              src={photo2}
-              width={279}
-              height={270}
-              placeholder={'blur'}
-              alt="Muller Arms Instagram Photo"
-            />
-          </div>
-          <div className={styles.img_wrapper}>
-            <Image
-              className={styles.instagram_photo}
-              src={photo3}
-              width={279}
-              height={270}
-              placeholder={'blur'}
-              alt="Muller Arms Instagram Photo"
-            />
-          </div>
-          <div className={styles.img_wrapper}>
-            <Image
-              className={styles.instagram_photo}
-              src={photo4}
-              width={279}
-              height={270}
-              placeholder={'blur'}
-              alt="Muller Arms Instagram Photo"
-            />
-          </div>
-        </div>
+        <Swiper
+          loop
+          spaceBetween={20}
+          className="mySwiper"
+          slidesPerView={'auto'}
+        >
+          <SwiperSlide>
+            <div className={styles.img_wrapper}>
+              <Image
+                className={styles.instagram_photo}
+                src={photo1}
+                width={267}
+                height={268}
+                placeholder={'blur'}
+                alt="Muller Arms Instagram Photo"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.img_wrapper}>
+              <Image
+                className={styles.instagram_photo}
+                src={photo2}
+                width={267}
+                height={268}
+                placeholder={'blur'}
+                alt="Muller Arms Instagram Photo"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.img_wrapper}>
+              <Image
+                className={styles.instagram_photo}
+                src={photo3}
+                width={279}
+                height={270}
+                placeholder={'blur'}
+                alt="Muller Arms Instagram Photo"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.img_wrapper}>
+              <Image
+                className={styles.instagram_photo}
+                src={photo4}
+                width={279}
+                height={270}
+                placeholder={'blur'}
+                alt="Muller Arms Instagram Photo"
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
 
         <a
           className={styles.instagram_btn}
